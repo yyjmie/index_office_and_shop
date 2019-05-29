607ec6814c57fe88b2721a71b84905b7b3eb63db
yyjmie <yyjmie@163.com>
2019-05-29 13:18:31 +0800

    指数表至2019年3月已经算完

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import calculate as cal
+import arrow
 
-this_month = '2018-01'
+month = '2018-01'
 
-cal.investment_index_calculate('office_index.csv', 'office_sale.csv', 'office_rent.csv', 'index_new.csv', this_month)
-cal.save_table('office_index.csv', 'index_new.csv', this_month)
+'''
+while month != '2019-04':
+	cal.investment_index_calculate('office_index.csv', 'office_sale.csv', 'office_rent.csv', 'index_new.csv', month)
+	cal.save_table('office_index.csv', 'index_new.csv', month)
+	month = arrow.get(month, 'YYYY-MM').shift(months=1).format('YYYY-MM')
+'''
+
+'''
+while month != '2019-04':
+	cal.investment_index_calculate('shop_index.csv', 'shop_sale.csv', 'shop_rent.csv', 'index_new.csv', month)
+	cal.save_table('shop_index.csv', 'index_new.csv', month)
+	month = arrow.get(month, 'YYYY-MM').shift(months=1).format('YYYY-MM')
+'''

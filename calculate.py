aa4150459a84f81d014b2a7cce8aa5397c5ad1ba
yyjmie <yyjmie@163.com>
2019-05-29 14:10:28 +0800

    所有指数表都补全到2019年3月了

--- calculate.py
+++ calculate.py
@@ -11,6 +11,7 @@ def save_table(old_file, new_file, this_month):
 
     df.to_csv(old_file)
 
+
 def investment_index_calculate(index_file, sale_file, rent_file, save_file, this_month):
     
     last_month = arrow.get(this_month,'YYYY-MM').shift(months=-1).format('YYYY-MM')
@@ -31,5 +32,30 @@ def investment_index_calculate(index_file, sale_file, rent_file, save_file, this
 
     # 保存
     merged.to_csv(save_file, columns=['city', this_month], index=False)
+
+
+def sale_or_rent_index_calculate(index_file, sale_file, save_file, this_month):
+    
+    last_month = arrow.get(this_month,'YYYY-MM').shift(months=-1).format('YYYY-MM')
+
+    df = pd.read_csv(index_file, usecols=['city', last_month])
+    df2 = pd.read_csv(sale_file, usecols=['city', last_month, this_month])
+    df2.rename(columns={this_month: "sale"+this_month, last_month: "sale"+last_month}, inplace=True)
+
+    # 合并
+    merged = pd.merge(df, df2, on='city')
+
+    # 计算
+    merged[this_month] = (merged['sale'+this_month]/merged['sale'+last_month])*merged[last_month]
+    merged[this_month] = merged[this_month].apply(lambda x: '{:.2f}'.format(x))
+
+    # 保存
+    merged.to_csv(save_file, columns=['city', this_month], index=False)
+
+
+def index_rate(index_file, save_file, this_month):
+
+	one_year_ago = arrow.get(this_month, 'YYYY-MM').shift(years=-1).format('YYYY-MM')
+	three_year_ago = arrow.get(this_month, 'YYYY')
 
 
--- main.py
+++ main.py
@@ -16,3 +16,29 @@ while month != '2019-04':
 	cal.save_table('shop_index.csv', 'index_new.csv', month)
 	month = arrow.get(month, 'YYYY-MM').shift(months=1).format('YYYY-MM')
 '''
+
+'''
+while month != '2019-04':
+	cal.sale_or_rent_index_calculate('office_sale_index.csv', 'office_sale.csv', 'index_new.csv', month)
+	cal.save_table('office_sale_index.csv', 'index_new.csv', month)
+	month = arrow.get(month, 'YYYY-MM').shift(months=1).format('YYYY-MM')
+'''
+
+'''
+while month != '2019-04':
+	cal.sale_or_rent_index_calculate('office_rent_index.csv', 'office_rent.csv', 'index_new.csv', month)
+	cal.save_table('office_rent_index.csv', 'index_new.csv', month)
+	month = arrow.get(month, 'YYYY-MM').shift(months=1).format('YYYY-MM')
+'''
+
+'''
+while month != '2019-04':
+	cal.sale_or_rent_index_calculate('shop_sale_index.csv', 'shop_sale.csv', 'index_new.csv', month)
+	cal.save_table('shop_sale_index.csv', 'index_new.csv', month)
+	month = arrow.get(month, 'YYYY-MM').shift(months=1).format('YYYY-MM')
+
+while month != '2019-04':
+	cal.sale_or_rent_index_calculate('shop_rent_index.csv', 'shop_rent.csv', 'index_new.csv', month)
+	cal.save_table('shop_rent_index.csv', 'index_new.csv', month)
+	month = arrow.get(month, 'YYYY-MM').shift(months=1).format('YYYY-MM')
+'''
